f64acdc0f3ee888b05a80c156468ff0bee7fd2ce
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-01-29 23:18:38 +0300

    added button that open dialog window(with mvvm)

---
+++ ViewModel/ApplicationViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using Pedometer.Model;
+
+namespace Pedometer.ViewModel
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Command openFileCommand;
+
+        public Command OpenFileCommand
+        {
+            get
+            {
+                if(openFileCommand == null)
+                {
+                    return openFileCommand = new Command(OpenFile);
+                }
+                else
+                {
+                    return openFileCommand;
+                }
+            }
+        }
+
+        private void OpenFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Json files (*.json)|*.json";
+            openFileDialog.ShowDialog();
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
