dc62003aeda254d25b20ea0ce6578036420b1e03
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-11-09 21:05:48 +0300

    update way of getting path for json reading

--- Services/Files/JsonFileParsing.cs
+++ Services/Files/JsonFileParsing.cs
@@ -1,24 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.Json;
+﻿using System.Collections.Generic;
 using System.IO;
+using System.Text.Json;
 
 namespace Pedometer.Model
 {
     public class JsonFileParsing : IFileService
     {
-        public List<List<Person>> Open(uint daysCount)
+        public List<List<Person>> Open(uint daysCount, string[] paths)
         {
             List<List<Person>> people = new List<List<Person>>();
             string json = "";
 
-            for(int i = 1; i <= daysCount; i++)
+            for (int i = 0; i < daysCount; i++)
             {
-
-                using(StreamReader files = new StreamReader($"day{i}.json"))
+                using (StreamReader files = new StreamReader(paths[i]))
                 {
                     json = files.ReadToEnd();
                     people.Add(JsonSerializer.Deserialize<List<Person>>(json));
