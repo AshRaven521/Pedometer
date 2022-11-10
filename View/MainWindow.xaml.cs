aca16e9429aab46f86a5030a7b03661eac0975bc
AshRaven521 <ivan.kondrashov.92@mail.ru>
2022-11-10 22:14:30 +0300

    delete unnecessary usings

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,20 +1,7 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using Pedometer.Model;
+using Pedometer.Services.Files.Export;
 using Pedometer.ViewModel;
-using Pedometer.Model;
+using System.Windows;
 
 namespace Pedometer
 {
@@ -26,7 +13,7 @@ namespace Pedometer
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new ApplicationViewModel(new JsonFileParsing(), new DefaultDialogService());
+            DataContext = new ApplicationViewModel(new JsonFileParsing(), new DefaultDialogService(), new FileExport());
         }
     }
 }
